a3d8343dac50ace8976f6735848c9d0e21309889
FinByz <info@finbyz.com>
2017-05-03 10:49:35 +0530

    Payment Entry function and hooks

---
+++ transport/api.py
@@ -0,0 +1,38 @@
+import frappe
+from frappe import _
+
+@frappe.whitelist()
+def payment_on_submit(self, method):
+	po_payments(self, method)
+
+@frappe.whitelist()
+def payment_on_cancel(self, method):
+	can_po_payments(self, method)
+
+
+
+#Update PO payments on Submit
+def po_payments(self, method):
+	for row in self.references:
+		if row.reference_doctype = "Purchase Order":
+			target_po = frappe.get_doc("Purchase Order", row.reference_name)
+			target_po.append("payments", {
+				"reference_date": self.reference_date,
+				"mode_of_payment": self.mode_of_payment,
+				"reference_no": self.reference_no,
+				"paid_amount" : self.allocated_amount,
+				"payment_entry" : self.name
+				"difference_amount" : self.difference_amount
+			})
+		target_po.save()
+		frappe.db.commit()
+
+#CANCEL Payment
+def can_po_payments(self, method):
+	for row in self.references:
+		if row.reference_doctype = "Purchase Order":
+			existing_row_id = frappe.db.get_value("Purchase Order Payments", filters={"parent": row.reference_name, "payment_entry": self.name}, fieldname="name")
+			frappe.delete_doc("Purchase Order Payments", existing_row_id)
+			frappe.db.commit()
+
+
