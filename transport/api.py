2c83747310a3a0fa6d3a1d5684f6b7d186ae9d3c
FinByz <info@finbyz.com>
2017-05-07 14:46:57 +0530

    minor correction

--- transport/api.py
+++ transport/api.py
@@ -33,7 +33,7 @@ def can_po_payments(self, method):
 	for row in self.references:
 		if row.reference_doctype == "Purchase Order":
 			existing_row_id = frappe.db.get_value("Purchase Order Payments", filters={"parent": row.reference_name, "payment_entry": self.name}, fieldname="name")
-			target_row = frappe.get_doc("Purchase Order Payments", row.target_row)
+			target_row = frappe.get_doc("Purchase Order Payments", existing_row_id)
 			target_row.docstatus = 2
 			frappe.delete_doc.delete_from_table("Purchase Order Payments", existing_row_id)
 			frappe.db.commit()
