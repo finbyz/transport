ca4453851cbca5cf621d0d1f7288e4aa712339b3
finbyz <info@finbyz.com>
2017-07-09 12:08:14 +0000

    deleted files

--- transport/transport/doctype/maintenance/maintenance.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2017, FinByz Tech Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Maintenance', {
-	refresh: function(frm) {
-
-	}
-});
--- transport/transport/doctype/truck_details/truck_details.js
+++
@@ -1,8 +0,0 @@
-// Copyright (c) 2016, FinByz Tech Pvt. Ltd. and contributors
-// For license information, please see license.txt
-
-frappe.ui.form.on('Truck Details', {
-	refresh: function(frm) {
-
-	}
-});
